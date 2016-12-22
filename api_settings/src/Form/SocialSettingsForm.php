b844aee5f47075d00a57266f760e4b961b8ef18e
Niels <niels@burst-digital.com>
2016-12-22 11:45:54 +0100

    add parent to yaml in socialform

--- api_settings/src/Form/SocialSettingsForm.php
+++ api_settings/src/Form/SocialSettingsForm.php
@@ -36,13 +36,13 @@ class SocialSettingsForm extends ConfigFormBase {
     $form['social_token'] = [
       '#type' => 'textfield',
       '#title' => 'Social admin token',
-      '#default_value' => $config->get('social_token'),
+      '#default_value' => $config->get('social.social_token'),
     ];
 
     $form['social_suffix'] = [
       '#type' => 'textfield',
       '#title' => 'Social suffix',
-      '#default_value' => $config->get('social_suffix'),
+      '#default_value' => $config->get('social.social_suffix'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -54,8 +54,8 @@ class SocialSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('api_settings.config');
 
-    $config->set('social_token', $form_state->getValue('social_token'));
-    $config->set('social_suffix', $form_state->getValue('social_suffix'));
+    $config->set('social.social_token', $form_state->getValue('social_token'));
+    $config->set('social.social_suffix', $form_state->getValue('social_suffix'));
     $config->save();
 
     parent::submitForm($form, $form_state);
