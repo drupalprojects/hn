883da81fe82aa4dc43a2b049299d135a3f5024ce
Niels <niels@burst-digital.com>
2016-12-23 17:47:21 +0100

    Rename PvmConfigSettingsForm to PvmSettingsForm

--- pvm_settings/src/Form/PvmSettingsForm.php
+++ pvm_settings/src/Form/PvmSettingsForm.php
@@ -14,7 +14,7 @@ class PvmSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'pvm_settings';
+    return 'api_settings_config';
   }
 
   /**
@@ -22,7 +22,7 @@ class PvmSettingsForm extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'pvm_settings.config',
+      'api_settings.config',
     ];
   }
 
@@ -30,6 +30,21 @@ class PvmSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('api_settings.config');
+
+    $form['show_share_buttons'] = [
+      '#type' => 'checkbox',
+      '#title' => 'Show share buttons',
+      '#default_value' => $config->get('show_share_buttons'),
+    ];
+
+    $form['countries_link'] = [
+      '#type' => 'textfield',
+      '#title' => 'Countries link',
+      '#default_value' => $config->get('countries_link'),
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -37,6 +52,12 @@ class PvmSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('api_settings.config');
+
+    $config->set('show_share_buttons', $form_state->getValue('show_share_buttons'));
+    $config->set('countries_link', $form_state->getValue('countries_link'));
+    $config->save();
+
     return parent::submitForm($form, $form_state);
   }
 
