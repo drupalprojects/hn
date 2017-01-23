9e22ac01f5fe45a0b225aebe8f521c24f7c8d8e9
Niels <niels@burst-digital.com>
2017-01-23 11:44:14 +0100

    Add api_url module

---
+++ api_url/src/FieldTrait.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Drupal\api_nodes;
+
+use Drupal\Core\Entity\Plugin\DataType\EntityReference;
+use Drupal\Core\Field\FieldItemList;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+
+/**
+ * Function for getting the fields from a node.
+ */
+trait FieldTrait {
+
+  /**
+   * This function gets all fields from a given node.
+   *
+   * @param \Drupal\Core\Entity\Entity|null $node
+   *   A entity which you want all fields from.
+   * @param array|null $returnArray
+   *   The array that should be returned.
+   *
+   * @return array
+   *   The full node.
+   */
+  private function getFullNode($node = NULL, $returnArray = array()) {
+    $moduleHandler = \Drupal::moduleHandler();
+
+    if ($node) {
+      // Get all the fields from the node.
+      foreach ($node->getFields() as $field) {
+        $name = $field->getName();
+
+        // Loop through all the values in a field.
+        foreach ($field as $value) {
+
+          // Check if the value is a entity reference.
+          if ($value instanceof EntityReferenceItem) {
+
+            // Loop through all the properties.
+            foreach ($value->getProperties(TRUE) as $property) {
+              // Check if property is a entityReference and not a type,
+              // because when it is a type the property->getValue()
+              // doesn't work.
+              // @TODO: Check if getValue() on type property in a not dirty way.
+              if ($property instanceof EntityReference && $name !== 'type') {
+                $property = $property->getValue();
+
+                $this->getReferencedNode($property, $name, $returnArray);
+              }
+
+              // Call hook if you want to return custom data for a entity
+              // reference value.
+              $moduleHandler->invokeAll('api_alter_entity_reference_data',
+                array(
+                  'property' => $property,
+                  'value' => $value,
+                  'returnArray' => &$returnArray[$name],
+                ));
+            }
+            // If type get value.
+            // @TODO: This is really dirty i think, should do it another way.
+            if ($name === 'type') {
+              $this->getValue($field, $returnArray[$name]);
+            }
+
+            continue;
+          }
+
+          $this->getValue($field, $returnArray[$name]);
+
+          $moduleHandler->invokeAll('api_alter_field_data',
+            array(
+              'value' => $value,
+              'returnArray' => &$returnArray[$name],
+            ));
+        }
+        $this->arrayOrObject($returnArray[$name]);
+      }
+    }
+    return $returnArray;
+  }
+
+  /**
+   * Get the value or target_id from a normal field.
+   *
+   * @param FieldItemList $field
+   *   The field the value/target_id should be obtained from.
+   * @param array|null $returnArray
+   *   A referenced array.
+   */
+  private function getValue(FieldItemList $field, &$returnArray) {
+    if (isset($field->value)) {
+      $returnArray[] = $field->value;
+    }
+    elseif (isset($field->target_id)) {
+      $returnArray[] = $field->target_id;
+    }
+  }
+
+  /**
+   * Get the full node for a referenced Item.
+   *
+   * @param \Drupal\Core\Entity\Entity|null $entity
+   *   A Entity you want the full node from.
+   * @param string $name
+   *   The name of the field.
+   * @param array|null $returnArray
+   *   The referenced array.
+   */
+  private function getReferencedNode($entity, $name, &$returnArray) {
+    if (method_exists($entity, 'getFields')) {
+      $node = $this->getFullNode($entity);
+      $returnArray[$name][] = $node;
+    }
+  }
+
+  /**
+   * This function checks if it should be a array.
+   *
+   * @param array|null $returnArray
+   *   A referenced array.
+   */
+  private function arrayOrObject(&$returnArray) {
+    if (is_array($returnArray) && count($returnArray) == 1) {
+      $returnArray = $returnArray[0];
+    }
+  }
+
+}
---
+++ api_url/src/Plugin/rest/resource/NodeRestResource.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace Drupal\api_nodes\Plugin\rest\resource;
+
+use Drupal\node\Entity\Node;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\language\Plugin\LanguageNegotiation\LanguageNegotiationUrl;
+use Drupal\rest\Plugin\ResourceBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "node_rest_resource",
+ *   label = @Translation("Node rest resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/v1/url"
+ *   }
+ * )
+ */
+class NodeRestResource extends ResourceBase {
+  use \Drupal\api_nodes\FileUrlsTrait;
+  use \Drupal\api_nodes\FieldTrait;
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  private $language;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->currentUser = $current_user;
+
+    $this->language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('api_nodes'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * Returns a list of bundles for specified entity.
+   *
+   * @return Response
+   *   Throws exception expected.
+   */
+  public function get() {
+    // Get the ?url= query.
+    return $this->getResponseByUrl(\Drupal::request()->get('url', ''));
+  }
+
+  /**
+   * Generate API response for given URL.
+   */
+  private function getResponseByUrl($url, $statusCode = 200) {
+    $url = '/' . trim($url, '/');
+
+    $language_negotiation = \Drupal::config('language.negotiation')->get('url');
+
+    if ($language_negotiation['source'] == LanguageNegotiationUrl::CONFIG_PATH_PREFIX) {
+
+      // The PATH_PREFIX method is used for language detection.
+      // This should be stripped of the url.
+      foreach ($language_negotiation['prefixes'] as $lang_id => $lang_prefix) {
+        if (empty($lang_prefix) && !isset($this->language)) {
+          $this->language = $lang_id;
+        }
+
+        if (!empty($lang_prefix) && strpos($url, $lang_prefix) === 1) {
+          // Change the language.
+          $this->language = $lang_id;
+
+          // Remove the prefix from the url.
+          $url = substr($url, strlen($lang_prefix) + 1);
+        }
+      }
+    }
+
+    // If the ?url= is empty, get the frontpage.
+    if ($url == '/' || empty($url)) {
+      $url = \Drupal::config('system.site')->get('page.front');
+    }
+
+    // Get the internal path (entity/entity_id) by the alias provided.
+    $path = \Drupal::service('path.alias_manager')->getPathByAlias($url, $this->language);
+
+    $response = NULL;
+
+    // Check if the entity is a node.
+    if (preg_match('/node\/(\d+)/', $path, $matches)) {
+
+      // Get the node.
+      $node = Node::load($matches[1]);
+      if (!$node) {
+        return $this->getErrorResponse(404, $url);
+      }
+
+      $node = $node->getTranslation($this->language);
+
+      // Check if the user has permissions to view this node.
+      if (!$node->access()) {
+        return $this->getErrorResponse(403, $url);
+      }
+
+      $nodeObject = $this->getFullNode($node);
+
+      $response = new \stdClass();
+      $response->content = $nodeObject;
+      $response->meta = $this->getMetatags($node);
+
+      $data = json_encode($response);
+      $httpResponse = new Response($data);
+
+      // Set status code.
+      if ($statusCode != 200) {
+        $httpResponse->setStatusCode($statusCode);
+      }
+
+      return $httpResponse;
+    }
+    if (!preg_match('/node\/(\d+)/', $path, $matches)) {
+      return $this->getErrorResponse(404, $url);
+    }
+
+    return $response;
+  }
+
+  /**
+   * Generate HTTP response for error page.
+   */
+  private function getErrorResponse($code, $originalUrl) {
+    $url = \Drupal::config('system.site')->get("page.$code");
+    if ($originalUrl == $url) {
+      // Error page is not found or accessible by itself.
+      // Prevent inifinite recursion.
+      switch ($code) {
+        case 403:
+          throw new AccessDeniedHttpException();
+
+        case 404:
+          throw new NotFoundHttpException();
+
+      }
+    }
+    return $this->getResponseByUrl($url, $code);
+  }
+
+  /**
+   * Get meta-tags for node.
+   *
+   * @param Node $node
+   *   Node.
+   *
+   * @return array|mixed
+   *   Returns all meta-tags.
+   */
+  protected function getMetatags(Node $node) {
+    $metatag_manager = \Drupal::service('metatag.manager');
+    $metatags = metatag_get_default_tags();
+    if ($metatags) {
+      foreach ($metatag_manager->tagsFromEntity($node) as $key => $value) {
+        $metatags[$key] = $value;
+      }
+      $token = \Drupal::token();
+      foreach ($metatags as $key => $value) {
+        $value = str_replace('[current-page:title]', '[node:title]', $value);
+        $metatags[$key] = $token->replace($value, [
+          'node' => $node,
+        ], [
+          'langcode' => $this->language,
+        ]);
+      }
+    }
+    if (!$metatags) {
+      $metatags = [];
+    }
+    $url = $node->toUrl('canonical');
+    $url->setOption('absolute', TRUE);
+    return $metatags + [
+      'description' => '',
+      'keywords' => '',
+      'canonical_url' => $url->toString(),
+    ];
+  }
+
+}
