e8a8fcd73b7ca5e346e8392e340aa288393d9861
Bart Langelaan <langelaanbart@gmail.com>
2017-05-01 11:21:37 +0200

    Add readme and api_url.api.php

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Headless drupal module
+
+## From beginning to end
+
+- User makes request to /url?
+- api_url NodeRestResource get()
+- getFullNode (FieldTrait.php)
+- api_url.module hook_alter_entity_reference_data
---
+++ api_url/api_url.api.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Hooks from the api_url module.
+ */
+
+/**
+ * This hook gets executed for every ReferencedField in an entity.
+ *
+ * @param $entity
+ *   Entity
+ * @param $value
+ *   Value which the entity comes from
+ * @param $returnArray
+ *   Referenced entity
+ */
+function hook_api_alter_entity_reference_data($entity, $value, &$returnArray) {
+
+}
+
+function hook_api_alter_field_data($value, &$returnArray) {
+
+}
+
+
