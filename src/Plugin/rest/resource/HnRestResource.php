15dbdc4bdd7457d15bb7787cebafcf5f50b0ea90
Bart Langelaan <langelaanbart@gmail.com>
2017-07-03 23:08:59 +0200

    Resolve EntityReferenceFields, and add them to the response

--- src/Plugin/rest/resource/HnRestResource.php
+++ src/Plugin/rest/resource/HnRestResource.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\hn\Plugin\rest\resource;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
 use Drupal\rest\ModifiedResourceResponse;
@@ -95,56 +97,77 @@ class HnRestResource extends ResourceBase {
     ];
 
     $path = \Drupal::request()->get('path', '');
-    $path = '/' . trim($path, '/');
 
     // TODO: Use LanguageNegotiationUrl:getLangcode to get the language from the path url
-
-    // TODO: Use Url::fromUserInput instead of Url:fromURI.
-    $url = Url::fromUri("internal:" . $path);
+    $url = Url::fromUri('internal:/' . trim($path, '/'));
     if(!$url->isRouted()) {
       throw new NotFoundHttpException('Entity not found for path '.$path);
     }
+
     $params = $url->getRouteParameters();
     $entity_type = key($params);
     if(!$entity_type) {
       throw new NotFoundHttpException('Path '.$path.' isn\'t an entity and is therefore not supported.');
     }
-    $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
 
+    $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
     $this->addEntity($entity);
 
     $this->response_data['paths'][$path] = $entity->uuid();
 
-    $response = new ModifiedResourceResponse($this->response_data);
-
-    return $response;
-  }
-
-  /**
-   * Responds to POST requests.
-   *
-   * Returns a list of bundles for specified entity.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
-   *   Throws exception expected.
-   */
-  public function post() {
-
-    // You must to implement the logic of your REST Resource here.
-    // Use current user after pass authentication to validate access.
-    if (!$this->currentUser->hasPermission('access content')) {
-      throw new AccessDeniedHttpException();
-    }
-
-    return new ModifiedResourceResponse("Implement REST State POST!");
+    return new ModifiedResourceResponse($this->response_data);
   }
 
   /**
    * @param \Drupal\Core\Entity\EntityInterface $entity
+   * @param string $view_mode
    */
-  private function addEntity($entity) {
+  private function addEntity($entity, $view_mode = 'default' ) {
+
+    // If this entity is already being added, don't add again.
+
+    if(isset($this->response_data['data'][$entity->uuid()])) return;
+
+    if(!$entity instanceof FieldableEntityInterface) return;
+
+
     $this->response_data['data'][$entity->uuid()] = $entity;
     $this->response_data['paths'][$entity->toUrl('canonical')->toString()] = $entity->uuid();
+
+    /**
+     * Find all fields that are hidden in this view
+     */
+    $display = entity_get_display($entity->getEntityTypeId(), $entity->bundle(), $view_mode);
+    $hidden_fields = array_keys($display->toArray()['hidden']);
+
+    /** @var $field \Drupal\Core\Field\FieldItemList */
+    foreach ($entity as $field_name => $field) {
+
+      /**
+       * Make sure we don't include hidden fields
+       */
+      if (in_array($field_name, $hidden_fields)) {
+        $entity->set($field_name, NULL);
+      }
+
+      /**
+       * If this field is an entity reference, add the referenced entities too.
+       */
+      else if ($field instanceof EntityReferenceFieldItemListInterface) {
+
+        // Get all referenced entities
+        $referenced_entities = $field->referencedEntities();
+
+        // Get the referenced view mode (e.g. teaser) that is set in the current display (e.g. full)
+        $referenced_entities_display = $display->getComponent($field_name);
+        $referenced_entities_view_mode = $referenced_entities_display && $referenced_entities_display['type'] === 'entity_reference_entity_view' ? $referenced_entities_display['settings']['view_mode'] : 'default';
+
+        foreach ($referenced_entities as $referenced_entity) {
+          $this->addEntity($referenced_entity, $referenced_entities_view_mode);
+        }
+
+      }
+    }
   }
 
 }
