3afca03edc7da478b29a4ffd9ec1919308f1b014
Niels <niels@burst-digital.com>
2017-08-08 14:08:25 +0200

    Set serializingParent on checking supportNormalizer in typeddatanormalizer

--- src/Normalizer/TypedDataNormalizer.php
+++ src/Normalizer/TypedDataNormalizer.php
@@ -26,6 +26,7 @@ class TypedDataNormalizer extends SerializationTypedDataNormalizer {
    */
   public function supportsNormalization($data, $format = NULL) {
     if ($this->serializingParent) {
+      $this->serializingParent = FALSE;
       // Let parent handle it.
       return FALSE;
     }
