bfad6120ec03bda3d872ce023dd2192fc9eef34a
Bart Langelaan <langelaanbart@gmail.com>
2017-09-24 23:36:53 +0200

    Issue #2909190 by bartlangelaan: Don't send data the client already has

---
+++ tests/src/Functional/HnCacheSessionTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\Tests\hn\Functional;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Provides some basic tests with the session cache.
+ *
+ * @group hn_cache_session
+ */
+class HnCacheSessionTest extends HnFunctionalTestBase {
+
+  public static $modules = [
+    'hn_cache_session',
+  ];
+
+  /**
+   * The internal node url.
+   *
+   * @var string
+   */
+  private $nodeUrl;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->makeEverythingReady();
+
+    $test_node = Node::create([
+      'type' => 'hn_test_basic_page',
+      'title' => 'Test node',
+    ]);
+
+    $test_node->save();
+
+    // We get the internal path to exclude the subdirectory the Drupal is
+    // installed in.
+    $this->nodeUrl = $test_node->toUrl()->getInternalPath();
+  }
+
+  public function testSessionTokens() {
+    $response = $this->getHnResponse($this->nodeUrl);
+    $response = json_decode($response, TRUE);
+
+    $this->assertEquals(
+      $response['data'][$response['paths'][$this->nodeUrl]]['title'],
+      'Test node'
+    );
+
+    $user = $response['__hn']['request']['user'];
+    $token = $response['__hn']['request']['token'];
+
+    $this->assertTrue(!empty($user));
+    $this->assertTrue(!empty($token));
+
+    // Do a second response with only the user set, this should return the same
+    // data as the first response.
+    $secondResponse = $this->getHnResponse($this->nodeUrl, ['_hn_user' => $user]);
+    $secondResponse = json_decode($secondResponse, TRUE);
+
+    $this->assertEquals(
+      $response['data'],
+      $secondResponse['data']
+    );
+
+    $this->assertEquals($user, $response['__hn']['request']['user']);
+
+    $responseWithoutData = $this->getHnResponse($this->nodeUrl, [
+      '_hn_user' => $user,
+      '_hn_verify' => [$token],
+    ]);
+    $responseWithoutData = json_decode($responseWithoutData, TRUE);
+
+    $this->assertTrue(!empty($responseWithoutData['data'][$response['paths'][$this->nodeUrl]]));
+    $this->assertTrue(empty($responseWithoutData['data'][$response['paths'][$this->nodeUrl]]['title']));
+  }
+
+}
--- tests/src/Functional/HnFunctionalTestBase.php
+++ tests/src/Functional/HnFunctionalTestBase.php
@@ -3,6 +3,8 @@
 namespace Drupal\Tests\hn\Functional;
 
 use Drupal\Tests\BrowserTestBase;
+use Drupal\user\Entity\Role;
+use Drupal\user\RoleInterface;
 
 /**
  * Provides helper methods for the HN module's functional tests.
@@ -19,8 +21,20 @@ abstract class HnFunctionalTestBase extends BrowserTestBase {
   /**
    * Gets an Hn Response from a path.
    */
-  protected function getHnResponse($path) {
-    return $this->drupalGet($this->getAbsoluteUrl('hn?_format=hn&path=' . urlencode($path)));
+  protected function getHnResponse($path, $options = []) {
+    $options = $options + ['_format' => 'hn', 'path' => $path];
+
+    return $this->drupalGet($this->getAbsoluteUrl('hn?' . http_build_query($options)));
+  }
+
+  /**
+   * Make sure the HN module is ready to go.
+   */
+  protected function makeEverythingReady() {
+    $anonymous = Role::load(RoleInterface::ANONYMOUS_ID);
+    $anonymous->grantPermission('restful get hn_rest_resource');
+    $anonymous->grantPermission('access content');
+    $anonymous->trustData()->save();
   }
 
 }
